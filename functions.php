091daf6c082b899c2ac2bc4560197002d5e85605
Christian Zetterholm <christian.zetterholm@utb.ecutbildning.se>
2021-11-11 11:09:49 +0100

    css changes

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ add_action('after_setup_theme', 'z_portfolio_theme_support');
 function z_portfolio_register_styles(){
     $version = wp_get_theme()->get('Version');
     wp_enqueue_style('z-portfolio-style', get_template_directory_uri() . '/css/style.css', array(), $version, 'all');
+    wp_enqueue_style('font-awesome', '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />', array(), $version, 'all');
 }
 
 add_action('wp_enqueue_scripts', 'z_portfolio_register_styles');
@@ -24,9 +25,30 @@ function z_portfolio_menus(){
 
 add_action('init', 'z_portfolio_menus');
 
-add_action( 'init', 'gp_register_taxonomy_for_object_type' );
+
 function gp_register_taxonomy_for_object_type() {
     register_taxonomy_for_object_type( 'post_tag', 'portfolio' );
 };
+
+add_action( 'init', 'gp_register_taxonomy_for_object_type' );
+
+function my_cptui_add_post_types_to_archives( $query ) {
+	if ( is_admin() || ! $query->is_main_query() ) {
+		return;    
+	}
+
+	if ( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
+		$cptui_post_types = cptui_get_post_type_slugs();
+
+		$query->set(
+			'post_type',
+			array_merge(
+				array( 'post' ),
+				$cptui_post_types
+			)
+		);
+	}
+}
+add_filter( 'pre_get_posts', 'my_cptui_add_post_types_to_archives' );
 
 ?>
