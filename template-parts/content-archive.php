fdf0c7ca5111926ac49eed30e831419faec8cecc
Christian Zetterholm <christian.zetterholm@utb.ecutbildning.se>
2021-11-11 15:08:47 +0100

    overhaul archive

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -1,6 +1,7 @@
 <article class="archive-box">
-    <img src="<?php the_post_thumbnail_url(); ?>" alt="" width="100%" height="auto">
-    <h1><?php the_title(); ?></h1>
-    <?php the_excerpt(); ?>
-    <p><a href="<?php the_permalink(); ?>">Läs mer</a></p>
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="" width="100%" height="auto">
+        <h1><?php the_title(); ?></h1>
+        <?php the_excerpt(); ?>
+        <p class="read-more"><a href="<?php the_permalink(); ?>">Läs mer</a></p>
+
 </article>
